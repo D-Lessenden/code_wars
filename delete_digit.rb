31e839b6a8ea6691835c1495ea44abf405c3d0a3
Daniel Lessenden <dless27@gmail.com>
2021-02-12 22:07:04 -0700

    level 6

--- delete_digit.rb
+++ delete_digit.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-def delete_digit(n)
-  str = n.to_s.chars
-  binding.pry 
-  str.combination(str.length - 1).uniq.sort.reverse[0].join.to_i
-end
+# def delete_digit(n)
+#   str = n.to_s.chars
+#   binding.pry
+#   str.combination(str.length - 1).uniq.sort.reverse[0].join.to_i
+# end
 
 # def delete_digit(n)
 #   num = n.to_s.split('')
@@ -19,10 +19,27 @@ end
 #   num.join()
 # end
 
-p delete_digit(334185)
-#34185
+def delete_digit(n)
+  array = n.to_s.split('').map(&:to_i)
+  var = []
+  results = []
+  array.count.times do
+    var << array.clone
+  end
+
+  var.each_with_index do |ele, ind|
+    ele.delete_at(ind)
+  end
+  var.each do |ele|
+    results << ele.join.to_i
+  end
+  results.sort.last
+end
+
 p delete_digit(152)
 #52
+p delete_digit(334185)
+#34185
 p delete_digit(1001)
 # # #101
 p delete_digit(10)
