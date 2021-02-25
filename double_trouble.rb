2796a413187883b701218cc03b048d10718dcd7d
Daniel Lessenden <dless27@gmail.com>
2021-02-25 11:49:03 -0700

    level 7, recursive solution

---
+++ double_trouble.rb
@@ -0,0 +1,40 @@
+require 'pry'
+# https://www.codewars.com/kata/57f7796697d62fc93d0001b8/train/ruby
+# level 7
+# def trouble(arr, t)
+#   if arr.combination(2).find{ |a,b| a+b == t}
+#     pair = arr.combination(2).find{ |a,b| a+b == t}
+#     arr.each_with_index do |num, ind|
+#       if pair[1] == num
+#         arr.delete_at(ind)
+#       end
+#     end
+#     trouble(arr, t)
+#   else
+#     arr
+#   end
+# end
+
+def trouble(arr, t)
+  arr.each_with_index do |num, ind|
+    if arr[ind+1] !=nil && num + arr[ind+1] == t
+      arr.delete_at(ind+1)
+      trouble(arr, t)
+    else
+      arr
+    end
+  end
+end
+
+
+p trouble([1, 2, 3, 4, 5],3)
+# [1, 3, 4, 5]
+#
+p trouble([1, 3, 5, 6, 7, 4, 3],7)
+# [1, 3, 5, 6, 7, 4]
+p trouble([4, 1, 1, 1, 4],2)
+# [4, 1, 4]
+p trouble([2, 2, 2, 2, 2, 2], 4)
+# [2]
+p trouble([2, 6, 2], 8)
+# [2, 2]
