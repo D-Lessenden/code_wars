45ca2a95a0238a0fa65899de7e8c2f77da13c2bc
Daniel Lessenden <dless27@gmail.com>
2021-02-24 21:04:59 -0700

    level 6

--- string_to_camel.rb
+++ string_to_camel.rb
@@ -1,5 +1,6 @@
 require 'pry'
 # https://www.codewars.com/kata/517abf86da9663f1d2000003/train/ruby
+# level 6
 def to_camel_case(str)
   # a = str.gsub(/[_,-](.)/) {"#{$1.upcase}"}
   # b = "this_is_a_string".gsub(/[_,-](.)/) {$1.upcase}
