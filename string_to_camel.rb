94d845283f0437d5fb0b2ff01a233b7367e75a7f
Daniel Lessenden <dless27@gmail.com>
2021-02-24 21:04:46 -0700

    level 6

---
+++ string_to_camel.rb
@@ -0,0 +1,19 @@
+require 'pry'
+# https://www.codewars.com/kata/517abf86da9663f1d2000003/train/ruby
+def to_camel_case(str)
+  # a = str.gsub(/[_,-](.)/) {"#{$1.upcase}"}
+  # b = "this_is_a_string".gsub(/[_,-](.)/) {$1.upcase}
+
+array = str.split(/[_,-]/)
+array.each_with_index do |word, ind|
+  if ind > 0
+    word.capitalize!
+  end
+end
+array.join
+end
+
+p to_camel_case("the_stealth_warrior")
+# #theStealthWarrior
+p to_camel_case("The-Stealth-Warrior")
+#TheStealthWarrior
