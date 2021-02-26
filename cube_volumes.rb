464688aaf9728fcbad09e0e39c32f01ed551e18b
Daniel Lessenden <dless27@gmail.com>
2021-02-26 12:39:41 -0700

    Refactor that clumsy solution... don't code while tired is the lesson learned

--- cube_volumes.rb
+++ cube_volumes.rb
@@ -2,17 +2,7 @@ require 'pry'
 # https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/ruby
 # level 8
 def find_difference(a, b)
-  array = []
-  x = a.inject(:*)
-  y = b.inject(:*)
-  if x > y
-    array << x
-    array << y
-  else
-    array << y
-    array << x
-  end
-  array[0] - array[1]
+  (a.inject(:*) - b.inject(:*)).abs
 end
 p find_difference([3, 2, 5], [1, 4, 4])
 # 14
